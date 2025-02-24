c5e79aa7c614e0b1721c811fc6ed0bbfe9048c8c
jmelendezr <jmelendezr@unal.edu.co>
2025-02-24 15:25:12 -0500

    gestiondear.py

--- gestiondear.py
+++ gestiondear.py
@@ -6,16 +6,13 @@ def compras_pais(archivo, pais):
         reader = csv.reader(f)
         next(reader)
         for row in reader:
-            if row[7] == pais:  # Índice de la columna 'pais'
+            if row[7] == pais:
                 conteo += 1
     return conteo
 
-# Solicitar al usuario el país
 archivo = "C:/Users/Estudiante/Documents/Nueva carpeta/SalesJan2009.csv"
 pais = input("Ingrese el nombre del país: ")
 
-# Contar transacciones en el país especificado
 conteo_transacciones = compras_pais(archivo, pais)
 
-# Mostrar resultado
 print(f"Número de transacciones en {pais}: {conteo_transacciones}")
